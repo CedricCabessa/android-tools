34d4f845df3f97175bbfd47838eb4e8cfcc8895a
Cédric Cabessa <ced@ryick.net>
2013-11-11 16:58:39 +0100

    add code from android-4.2.2_r1.1
    build/libs/host
    frameworks/base/libs/androidfw
    frameworks/base/tools/aapt
    frameworks/native/include
    framework/native/libs/utils
    system/core/include/android
    system/core/include/androidfw
    system/core/include/corkscrew
    system/core/include/system
    system/core/liblog

---
+++ AndroidConfig.h
@@ -0,0 +1,10 @@
+/**
+ * Android build system include a similar file in all c/c++ compilation
+ */
+
+#ifndef _ANDROID_CONFIG_H
+#define _ANDROID_CONFIG_H
+
+#define OS_PATH_SEPARATOR '/'
+
+#endif /* _ANDROID_CONFIG_H */
